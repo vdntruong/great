ccedc921a4695bcbc9c53b223d1c7adf02f96fcd
vdntruong <truongvodainhat@gmail.com>
2025-03-15 23:29:09 +0700

    reorder health check writer status, content-type

--- services/commons/handler/healthz.go
+++ services/commons/handler/healthz.go
@@ -8,15 +8,17 @@ import (
 
 func HealthCheck(startedAt time.Time, serviceName string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		uptime := time.Since(startedAt)
-		resp := map[string]interface{}{
-			"uptime":      uptime.String(),
-			"started_at":  startedAt.String(),
-			"serviceName": serviceName,
+		var uptime = time.Since(startedAt)
+		var data = map[string]interface{}{
+			"uptime":       uptime.String(),
+			"started_at":   startedAt.String(),
+			"service_name": serviceName,
 		}
-		w.WriteHeader(http.StatusOK)
+
 		w.Header().Set("Content-Type", "application/json")
-		if err := json.NewEncoder(w).Encode(resp); err != nil {
+		w.WriteHeader(http.StatusOK)
+
+		if err := json.NewEncoder(w).Encode(data); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 		return
